c33d53bc2388116a3eb27f47db69f741683a95de
= <=>
2023-10-05 22:23:08 +0700

    devkh

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="theme-color" content="#000000">
+    <meta name="theme-color" content="#6236FF ">
     <title>Tulang Bawang - Lampung Fair 2023</title>
     <meta name="description" content="Finapp HTML Mobile Template">
     <meta name="keywords"
